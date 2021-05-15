d0d9a3c16653f7851ae07a38a61b6030798b74fe
Kalpesh Khandla <khandlakalpesh92@gmail.com>
2021-05-15 12:28:33 -0800

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/facebook_messenger_clone_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.facebook_messenger_clone_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:facebook_messenger_clone_app/screens/home_page_screen.dart';
+import 'package:flutter/material.dart';
+
+
+/*
+Title:Entry Point of a App
+Purpose:Entry Point of a App
+Created By:Kalpesh Khandla
+*/
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePageScreen(),
+    );
+  }
+}
